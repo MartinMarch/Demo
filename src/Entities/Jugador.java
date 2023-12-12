d42f56a9b807843da600e4fc8474036b6b568dba
Martin March <mmarchrovira@gmail.com>
2023-12-12 22:02:16 +0100

    Implementadas phisicas entrenador Pokemon

---
+++ src/Entities/Jugador.java
@@ -0,0 +1,75 @@
+package Entities;
+import java.awt.*;
+import javax.swing.ImageIcon;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Jugador {
+    private int x, y;
+    private Image[] imagenes;
+    private int indiceImagenActual;
+    private char ultimaDireccion = ' ';
+    private int[] option;
+    private int i = 0;
+
+
+
+    public Jugador(int x, int y, String[] rutasImagenes) {
+        this.option = new int[3];
+        for(int i = 0; i < 3; i++){
+            option[i] = i;
+        }
+
+        this.x = x;
+        this.y = y;
+        this.imagenes = new Image[rutasImagenes.length];
+        for (int i = 0; i < rutasImagenes.length; i++) {
+            this.imagenes[i] = new ImageIcon(rutasImagenes[i]).getImage();
+        }
+        this.indiceImagenActual = 0; // Inicia con la primera imagen
+    }
+
+    public void mover(char direccion, int velocidad) {
+        if (ultimaDireccion != direccion) {
+            ultimaDireccion = direccion;
+            if (direccion == 'A') indiceImagenActual = 2;
+            if (direccion == 'D') indiceImagenActual = 3;
+            if (direccion == 'W') indiceImagenActual = 1;
+            if (direccion == 'S') indiceImagenActual = 0;
+        } else if (direccion == 'S') {
+            if(i == 3){i = 0;}if (option[i] == 0) {indiceImagenActual = 4;} else if (option[i] == 1) {indiceImagenActual = 0;} else if (option[i] == 2) {indiceImagenActual = 5; }i++;
+        }else if (direccion == 'D') {
+            if(i == 3){i = 0;}if (option[i] == 0) {indiceImagenActual = 6;} else if (option[i] == 1) {indiceImagenActual = 3;} else if (option[i] == 2) {indiceImagenActual = 7; }i++;
+        }else if (direccion == 'A') {
+            if(i == 3){i = 0;}if (option[i] == 0) {indiceImagenActual = 8;} else if (option[i] == 1) {indiceImagenActual = 2;} else if (option[i] == 2) {indiceImagenActual = 9; }i++;
+        }else if (direccion == 'W') {
+            if(i == 3){i = 0;}if (option[i] == 0) {indiceImagenActual = 10;} else if (option[i] == 1) {indiceImagenActual = 1;} else if (option[i] == 2) {indiceImagenActual = 11; }i++;
+        }
+
+
+        switch (direccion) {
+                case 'A':
+                    x -= velocidad;
+                    break;
+                case 'D':
+                    x += velocidad;
+                    break;
+                case 'W':
+                    y -= velocidad;
+                    break;
+                case 'S':
+                    y += velocidad;
+                    break;
+            }
+        }
+
+        public void dibujar (Graphics g){
+            g.drawImage(imagenes[indiceImagenActual], x, y, null);
+        }
+
+        // Métodos getters y setters para x, y...
+    }
+
+
+
--- src/Logic/Game.java
+++ src/Logic/Game.java
@@ -1,38 +1,65 @@
 package Logic;
+import Entities.Jugador;
+
 import javax.swing.JFrame;
 import java.awt.Graphics;
 import javax.swing.JPanel;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Game extends JPanel {
 
-    // Método para actualizar el estado del juego
-    private void update() {
-        // Lógica de actualización del juego
+    private Image backgroundImage;
+    private Jugador jugador;
+
+    public Game() {
+        backgroundImage = new ImageIcon("CarpetaDeRecursos/escenari.jpg").getImage();
+        String[] rutasJugador = {"CarpetaDeRecursos/J1/j1MirandoHaciaDelante.png", "CarpetaDeRecursos/J1/j1MirandoHaciaDetras.png", "CarpetaDeRecursos/J1/j1MirandoHaciaIzquierda.png", "CarpetaDeRecursos/J1/j1MirandoHaciaDerecha.png",
+                "CarpetaDeRecursos/J1/j1CaminaHaciaDelante1.png","CarpetaDeRecursos/J1/j1CaminaHaciaDelante2.png"
+                ,"CarpetaDeRecursos/J1/j1CaminaHaciaDerecha2.png","CarpetaDeRecursos/J1/j1CaminaHaciaDerecha1.png"
+                ,"CarpetaDeRecursos/J1/j1CaminaHaciaIzquierda2.png", "CarpetaDeRecursos/J1/j1CaminaHaciaIzquierda1.png"
+                ,"CarpetaDeRecursos/J1/j1CaminaHaciaDetras2.png", "CarpetaDeRecursos/J1/j1CaminaHaciaDetras1.png"};
+        jugador = new Jugador(100, 100, rutasJugador);
+
+
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int velocidad = 10;
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_A: jugador.mover('A', velocidad); break;
+                    case KeyEvent.VK_D: jugador.mover('D', velocidad); break;
+                    case KeyEvent.VK_W: jugador.mover('W', velocidad); break;
+                    case KeyEvent.VK_S: jugador.mover('S', velocidad); break;
+                }
+                repaint();
+            }
+        });
+        setFocusable(true);
     }
 
     @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        // Dibuja los gráficos del juego aquí
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+        jugador.dibujar(g);
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Mi Juego 2D");
+        JFrame frame = new JFrame("Mi Juego 2D con Jugador");
         Game game = new Game();
         frame.add(game);
         frame.setSize(800, 600);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Bucle principal del juego
-        while (true) {
-            game.update();
-            game.repaint();
-            try {
-                Thread.sleep(10); // Controla la velocidad del bucle
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
